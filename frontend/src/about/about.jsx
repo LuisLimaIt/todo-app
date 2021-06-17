178d9181d486ebbd5472cbe2284c097ae32cdfe5
LuisLimaIt <pls.luis@gmail.com>
2021-06-17 14:20:58 -0300

    Componente PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import PageHeader from '../template/pageHeader';
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name="Sobre" small="o Projeto" />
+        <h2>Curso React + Redux (Projeto - Lista de Tarefas)</h2>
+        <p>Projeto desenvolvido durante o curso <strong>Curso React + Redux: Fundamentos e 2 Apps do Absoluto ZERO!</strong></p>
+        <p><strong>Professor: </strong> Leonardo Leitão - Cod3r</p>
+
+        <p><strong>Aluno: </strong> Luis Paulo de Lima Soares</p>
+        <p><strong>Github: </strong> <a href="https://github.com/LuisLimaIt">https://github.com/LuisLimaIt</a></p>
+        <p><strong>Linkedin: </strong> <a href="https://www.linkedin.com/in/luisplsoares/">https://www.linkedin.com/in/luisplsoares/</a></p>
+    
     </div>
 );
