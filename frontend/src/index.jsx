45db90f4dbb47ea2f1f9043665a3d4d0f7ed6cd7
LuisLimaIt <pls.luis@gmail.com>
2021-07-19 13:13:02 -0300

    Aplicando Middleware (Redux-Multi)

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+
 import promise from 'redux-promise';
+import multi from 'redux-multi';
 
 import App from './main/app';
 import reducers from './main/reducers';
